3be7a65864a98072e1ecdb5a329c89163d0a5844
YunhaoZZ <zhangenochyun@outlook.com>
2023-04-04 22:54:26 -0500

    removed https, using EC2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require("path");
 const cors = require('cors');
 
 const corsOptions ={
-    origin:'https://localhost:3000', 
+    origin:'http://localhost:3000', 
     // origin:'https://yunhaozz.github.io', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
